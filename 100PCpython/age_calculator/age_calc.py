4930498af44de2a11f03d51a944625e398d161af
Gabriel Cavalcante <gabrielcavalcante1021@gmail.com>
2021-04-29 22:00:46 -0300

    Refactoring code.
    Making things less ugly.

--- 100PCpython/age_calculator/age_calc.py
+++ 100PCpython/age_calculator/age_calc.py
@@ -6,12 +6,12 @@ from datetime import datetime
 
 
 class Calc_age:
-    def calc_age(self):
+    def calc_age():
         print("===================================================")
         print("Program to calculate a person's age or the year they were born.")
 
-        ex = False
-        while not ex:
+        exit = False
+        while not exit:
 
             # Offers the options finding out a person's age or the year they were born.
             # Makes sure that the option is not in capital letters
@@ -19,24 +19,45 @@ class Calc_age:
                 "Would you like to find out a person's age (a) or the year they were born (b) ?\n").lower()
             # If option 'a' is chosen
             if opt == "a":
-                year_born: int = int(input("What year was this person born in? \n"))
-                full_date = datetime.now()
-                year = int(full_date[0])
-                age = year - year_born
-                print("This person has between {} and {} years of age.".format(age-1, age))
+                person_age = age()
+                print(
+                    "This person is between {} and {} years of age.".format(person_age-1, person_age))
             # If option 'b' is chosen
             elif opt == "b":
-                age = input("Whats the person's age? \n")
-                full_date = datetime.now()
-                year = int(full_date[0])
-                print("This person was born in {}n".format(year - int(age)))
+                born_year = year()
+                print("This person was born in {}n".format(born_year))
             # If the input is incorrect
             else:
                 print("Option unavailable.")
 
             question: str = input(
-                "Would you like to continue to calculate a person's age? (y/n) \n").lower()
+                "Would you like to continue to calculate someone's age? (y/n) \n").lower()
 
             if question == "n" or question == "no":
-                ex = True
+                exit = True
         print("End of the program...\n===================================================")
+
+    # Function to calculate a person's age.
+    def age():
+        year_born: int = int(
+            input("What year was this person born in? \n"))
+        full_date = datetime.now()
+        year = int(full_date[0])
+        years_old = int(year - year_born)
+        return years_old
+
+    # Function to calculate the year of birth.
+    def year():
+        age = input(input("Whats the person's age? \n"))
+        full_date = datetime.now()
+        year = int(full_date[0])
+        born_in = int(year - int(age))
+        return born_in
+
+    # Calls the main function
+    calc_age()
+
+
+# New instance of the Calc_age class
+calculate_age = Calc_age()
+calculate_age()
--- 100PCpython/temperature_converter/temperature_converter.py
+++ 100PCpython/temperature_converter/temperature_converter.py
@@ -1,59 +1,152 @@
-# Program to convert temperature to different scales
+"""
+Program to convert temperature to different scales.
+
+By Gabriel Cavalcante
+"""
+
+# Main function
 
 
 def temp_conv():
     print("=========================================================\n" +
-          "Program to convert temperature to different scales by Gabriel Cavalcante\n" +
+          "Program to convert temperatures to different scales by Gabriel Cavalcante\n" +
           ":")
+
+    # Variable do decide if the program continues to operate or not.
     close = False
     while not close:
         scale = input(
-            "Select the desired scale:\n(c) for Celsius.\n(k) for Kelvin.\n(f) for Farenheit.\n-> ").lower()
+            "Select the desired scale:\n(c) for Celsius.\n(k) for Kelvin.\n(f) for Fahrenheit.\n-> ").lower()
         temperature = float(input("Type the degree in {}: ".format(scale)))
 
         # If the scale is Celsius
         if scale == "c":
-            opt = input(
-                "What do you wish to convert to:\n(k) for Kelvin.\n(f) for Farenheit.\n-> ").lower()
-            if opt == "k":
-                print("{}ºC para Kelvin: ".format(temperature))
-                c_to_k = (temperature + 273.15)
-                print(c_to_k)
-            if opt == "f":
-                print("{}ºC para Farenheit: ".format(temperature))
-
-                print(float((1.8*temperature) + 32))
-
+            fromCelsius(temperature)
         # If the scale is Kelvin
         elif scale == "k":
-            opt = input(
-                "What do you wish to convert to:\n(c) for Celsius.\n(f) for Farenheit.\n-> ")
-            if opt == "c":
-                print("{}ºK para Celsius: ".format(temperature))
-                print("{}ºC".format(float(temperature - 273.15)))
-            if opt == "f":
-                print("{}ºK para Farenheit: ".format(temperature))
-                print("{}ºF".format(float(1.8 * (temperature - 273) + 32)))
+            fromKelvin(temperature)
 
         # If the scale is Fahrenheit
         elif scale == "f":
-            opt = input(
-                "What do you wish to convert to:\n(c) for Celsius.\n(k) for Kelvin.\n-> ")
-            if opt == "c":
-                print("{}ºF para Celsius: ".format(temperature))
-                print("{}ºC".format(float((temperature-32)/1.8)))
-            if opt == "k":
-                print("{}ºF para Kelvin: ".format(temperature))
-                print("{}ºK".format(float((temperature + 459.67)*(5/9))))
+            fromFahrenheit(temperature)
 
         # Asks if the user wishes to continue to operate the program
         closure = input(
-            "Do you wish to continue to convert temperatures?('y' or 'n'): ")
+            "Do you wish to continue to convert temperatures?('y' or 'n'): ").lower()
         # If the answer is no
         if closure == "n":
             close = True  # Program ends in this condition
             print("End of the program.")
 
 
-# Initiate the program
+# Function for the Celsius conversion
+def fromCelsius(temp):
+    '''
+    This function triggers if the user chooses to convert the temperature from Celsius.
+    It takes an entry (temp) and asks to what scale the user wishes to convert to.
+    '''
+    opt = input(
+        "What do you wish to convert it to:\n(k) for Kelvin.\n(f) for Fahrenheit.\n-> ").lower()
+    if opt == "k":
+        celsiusToKelvin(temp)
+    if opt == "f":
+        celsiusToFahrenheit(temp)
+
+# Function for the Kelvin conversion
+
+
+def fromKelvin(temp):
+    '''
+    This function triggers if the user chooses to convert the temperature from Kelvin.
+    It takes an entry (temp) and asks to what scale the user wishes to convert to.
+    '''
+    opt = input(
+        "What do you wish to convert to:\n(c) for Celsius.\n(f) for Fahrenheit.\n-> ").lower()
+    if opt == "c":
+        kelvinToCelsius(temp)
+    if opt == "f":
+        kelvinToFahrenheit(temp)
+
+# Function for the Fahrenheit conversion
+
+
+def fromFahrenheit(temp):
+    '''
+    This function triggers if the user chooses to convert the temperature from Fahrenheit.
+    It takes an entry (temp) and asks to what scale the user wishes to convert to.
+    '''
+    opt = input(
+        "What do you wish to convert to:\n(c) for Celsius.\n(k) for Kelvin.\n-> ").lower()
+    if opt == "c":
+        fahrenheitToCelsius(temp)
+    if opt == "k":
+        fahrenheitToKelvin(temp)
+
+
+# Function to calculate the conversion from Celsius to Kelvin
+def celsiusToKelvin(temp):
+    '''
+    This function calculates the conversion from Celsius to Kelvin.
+    '''
+    print("{}ºC para Kelvin: ".format(temp))
+    c_to_k = float(temp + 273.15)
+    print(c_to_k)
+
+# Function to calculate the conversion from Celsius to Fahrenheit
+
+
+def celsiusToFahrenheit(temp):
+    '''
+    This function calculates the conversion from Celsius to Fahrenheit.
+    '''
+    print("{}ºC para Fahrenheit: ".format(temp))
+    c_to_f = float((1.8*temp) + 32)
+    print(c_to_f)
+
+# Function to calculate the conversion from Kelvin to Fahrenheit
+
+
+def kelvinToFahrenheit(temp):
+    '''
+    This function calculates the conversion from Kelvin to Fahrenheit.
+    '''
+    print("{}ºK para Fahrenheit: ".format(temperature))
+    k_to_f = float(1.8 * (temperature - 273) + 32)
+    print("{}ºF".format(k_to_f))
+
+# Function to calculate the conversion from Kelvin to Celsius
+
+
+def kelvinToCelsius(temp):
+    '''
+    This function calculates the conversion from Kelvin to Celsius.
+    '''
+    print("{}ºK para Celsius: ".format(temp))
+    k_to_c = float(temp - 273.15)
+    print("{}ºC".format(k_to_c))
+
+# Function to calculate the conversion from Fahrenheit to Celsius
+
+
+def fahrenheitToCelsius(temp):
+    '''
+    This function calculates the conversion from Fahrenheit to Celsius.
+    '''
+    print("{}ºF para Celsius: ".format(temperature))
+    f_to_c = float((temperature-32)/1.8)
+    print("{}ºC".format(f_to_c))
+
+# Function to calculate the conversion from Fahrenheit to Kelvin
+
+
+def fahrenheitToKelvin(temp):
+    '''
+    This function calculates the conversion from Fahrenheit to Kelvin.
+    '''
+    print("{}ºF para Kelvin: ".format(temp))
+    f_to_k = float((temp + 459.67)*(5/9))
+    print("{}ºK".format(f_to_k))
+
+
+# Initiates the program
 temp_conv()
